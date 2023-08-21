b0271f329d9a9922c88e1159fd83835343019d3f
thalisson-sousa <thalissonpop91@gmail.com>
2023-08-21 00:18:30 -0300

    feat: Criando pagina de favorito

---
+++ agendaDeContatos/src/pages/favoritos/Favoritos.jsx
@@ -0,0 +1,9 @@
+import imagem from './favoritos.svg'
+
+export default function Favoritos() {
+    return (
+        <div>
+            <img src={imagem} style={{ height: 400, marginTop: 150 }} />
+        </div>
+    )
+}
