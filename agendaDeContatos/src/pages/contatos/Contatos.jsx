d29b2eb4d6d6ae24e5588455d378e5473168ff12
thalisson-sousa <thalissonpop91@gmail.com>
2023-08-21 00:16:14 -0300

    Criando pagina de contatos

---
+++ agendaDeContatos/src/pages/contatos/Contatos.jsx
@@ -0,0 +1,47 @@
+import styles from "./Contatos.module.css";
+import CriaContato from "../../components/linkCriaContato/LinkCriaContato";
+import Contato from '../../components/Contatos/Contato';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+async function getApi() {
+  return await axios.get('https://agendadecontatosapi.onrender.com/contatos');
+}
+
+export default function Contatos() {
+  const { isLoading, isError, data } = useQuery(['contacts'], () => getApi());
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (isError) {
+    return <p>Error loading data</p>;
+  }
+
+  if (!data || !data.data || data.data.length === 0) {
+    return <p>No data available</p>;
+  }
+
+  return (
+    <div className={styles.container}>
+      <section>
+        <CriaContato />
+      </section>
+      <section>
+      <div className={styles.Corpo}>
+      {data.data.map((contato) => (
+        <Contato 
+        key={contato.id} 
+        id={contato.id} 
+        imgURL={contato.imgURL} 
+        nome={contato.nome} 
+        sobrenome={contato.sobrenome}
+        bgColor={"#" + ((1 << 24) * Math.random() | 0).toString(16)}
+        />
+      ))}
+    </div>
+      </section>
+    </div>
+  );
+}
