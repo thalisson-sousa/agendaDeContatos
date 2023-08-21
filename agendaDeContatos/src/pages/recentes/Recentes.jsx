5d7131fdb0cdd55a46c8f21d0dc5eb2335333fb2
thalisson-sousa <thalissonpop91@gmail.com>
2023-08-21 00:18:52 -0300

    feat: Criando pagina de Recentes

---
+++ agendaDeContatos/src/pages/recentes/Recentes.jsx
@@ -0,0 +1,9 @@
+import imagem from './notFound.svg'
+
+export default function Recentes() {
+  return (
+    <div>
+      <img src={imagem} style={{ height: 400, marginTop: 150 }} />
+    </div>
+  );
+}
