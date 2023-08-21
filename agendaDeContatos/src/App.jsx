43b63ecd1313cc030eefef2a5237fd8c55ef2ecb
thalisson-sousa <thalissonpop91@gmail.com>
2023-08-21 00:19:13 -0300

    feat: Criando Roteamento das Paginas

--- agendaDeContatos/src/App.jsx
+++ agendaDeContatos/src/App.jsx
@@ -1,11 +1,26 @@
-import './App.css'
-import Contatos from './pages/Contatos'
+import './App.css';
+import Search from './components/search/Search';
+import Contatos from './pages/contatos/Contatos';
+import FooterLinks from './components/footer/FooterLinks';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Favoritos from './pages/favoritos/Favoritos';
+import Recentes from './pages/recentes/Recentes';
 
 function App() {
 
   return (
     <>
-      <Contatos />
+    <BrowserRouter>
+      <Search />
+      <Routes>
+        <Route path='/' element={<Contatos/>} />
+        <Route path="favoritos" element={<Favoritos />} />
+        <Route path="recentes" element={<Recentes />} />
+      </Routes>
+      <section className="footer">
+        <FooterLinks />
+      </section>
+    </BrowserRouter>
     </>
   )
 }
